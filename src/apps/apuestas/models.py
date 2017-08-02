aea7273856a4586ee98768240952e0a96c3c6e1f
usuario <usuario@debian>
2017-08-02 18:21:12 -0300

    Commit Inicial

--- src/apps/apuestas/models.py
+++ src/apps/apuestas/models.py
@@ -2,5 +2,33 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Questions(models.Model):
+    text = models.CharField(max_length = 200)
+    due_date = models.DateTimeField('Fecha Limite')
+    create_date = models.DateTimeField('fecha publicacion', auto_now_add = True)
+    create_user = models.ForeignKey(User, related_name = "pregunta_create_user")
+    update_date = models.DateTimeField('fecha publicacion', auto_now_add = True)
+    update_user = models.ForeignKey(User, related_name = "pregunta_update_user")
+
+    def __unicode__(self):
+        return "%s" % (self.text)
+    @property
+    def respuestas(self):
+        return RespuestasValidas.objects.filter(pregunta=self)
+
+class RespuestasValidas(models.Model):
+    pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    create_date = models.DateTimeField('fecha publicacion', auto_now_add = True)
+    create_user = models.ForeignKey(User, related_name = "respuesta_create_user")
+    update_date = models.DateTimeField('fecha publicacion', auto_now_add = True)
+    update_user = models.ForeignKey(User, related_name = "respuesta_update_user")
+
+    class Meta:
+        unique_together = ('pregunta', 'text')
+
+    def __unicode__(self):
+        return "%s" % (self.text)
--- src/apuestas/settings.py
+++ src/apuestas/settings.py
@@ -71,15 +71,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'apuestas.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
@@ -119,3 +110,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *
+except:
+    pass
+#after import local_settings
