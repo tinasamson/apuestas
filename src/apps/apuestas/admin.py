6508e44f4bd5ea07a88b88917fc1076a116e1ef3
usuario <usuario@debian>
2017-08-02 20:23:22 -0300

    Commit Inicial

--- src/apps/apuestas/admin.py
+++ src/apps/apuestas/admin.py
@@ -2,5 +2,28 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Pregunta, RespuestasValidas
+
+def add_simbol_text(modeladmin, request, queryset):
+    for q in queryset:
+        q.text = q.text + '?'
+        q.save()
+add_simbol_text.short_description = 'Agregar simbolo ?'
+
+class RespuestasValidasHeadInLine(admin.TabularInline):
+    model=RespuestasValidas
+    extra = 1
+    readonly_fields = ('create_user', 'update_user')
+
+class PreguntaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'create_user')
+    search_fields = ('text', 'create_user__username')
+    list_filter = ('text',)
+    inlines = [RespuestasValidasHeadInLine, ]
+    actions = [add_simbol_text, ]
+
+admin.site.register(Pregunta, PreguntaAdmin)
+admin.site.register(RespuestasValidas)
+
 
 # Register your models here.
--- src/apps/apuestas/models.py
+++ src/apps/apuestas/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Questions(models.Model):
+class Pregunta(models.Model):
     text = models.CharField(max_length = 200)
     due_date = models.DateTimeField('Fecha Limite')
     create_date = models.DateTimeField('fecha publicacion', auto_now_add = True)
