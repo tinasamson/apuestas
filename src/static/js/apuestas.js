81e8c76f7afce63dc6e3facf94a06c85faca1718
usuario <usuario@debian>
2017-08-30 20:22:30 -0300

    clase 30/08

--- src/static/js/apuestas.js
+++ src/static/js/apuestas.js
@@ -1,5 +1,22 @@
+function open_modal_register(){
+    $('#modal_register').modal('show');
+}
 
-function send_apuestas(respuesta_valida_id){
-  alert(respuesta_valida_id)
+function cancel_modal_register(){
+  $('modal_register').find('#id_username').val();
+  $('modal_register').find('#id_password1').val();
+  $('modal_register').find('#id_password2').val();
+  $('modal_register').model('hide');
+}
 
+function send_register_modal() {
+
+  $.ajax({
+    type: "POST",
+    url: $('#id_form_register').atrr('action'),
+    data: $('#id_form_register').serialize(),
+    succes: function(response){
+      alert(response)
+    }
+  })
 }
