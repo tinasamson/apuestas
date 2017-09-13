809436dde82774ad2dc82951cd39ab7902476e39
tina <tinab.samson@gmail.com>
2017-09-13 01:36:25 -0300

    fab1

--- src/fabfile.py
+++ src/fabfile.py
@@ -2,8 +2,12 @@ from fabric.api import env
 from fabric.api import run, prefix
 from fabric.context_managers import cd
 #from fabric.operations import sudo
-
+from fab_tools.fab_settings import (
+    USER, PASSWORD, PATH_PROJECT, PATH_PROJECT_SRC,
+    PATH_VENV, PATH_VENV_ACTIVATE
+)
 
 def test():
-    env.host = [""]
-    env.user = []
+    env.host = ["tsamson.tuxis.com.ar"]
+    env.user = USER["test"]
+    env.password 
